ea22ad316ea2d41b1d6900a68c0e3dbebc845f06
Sotaro KARASAWA <sotaro.k@gmail.com>
2015-07-15 01:40:25 +0900

    Add application test

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -5,14 +5,23 @@
 
 namespace Dietcube;
 
+use Pimple\Container;
+
+/**
+ * @backupGlobals
+ */
 class ApplicationTest extends \PHPUnit_Framework_TestCase
 {
     public function testInstantiate()
     {
-        $app = $this->getMockForAbstractClass(
-            '\\Dietcube\\Application',
-            [__DIR__, 'development']
-        );
+        $_SERVER['HTTP_HOST'] = 'www.dietcube.org';
+        $_SERVER['SERVER_PORT'] = '80';
+        $_SERVER['REQUEST_URI'] = '/documentation/setup';
+        $container = new Container();
+        $container['global.server'] = new Parameters($_SERVER);
+
+        $app = new MockApplication(__DIR__, 'development');
+
         $this->assertEquals(__DIR__, $app->getAppRoot());
         $this->assertEquals('development', $app->getEnv());
         $this->assertEquals([
@@ -21,5 +30,28 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
             ],
             $app->getConfigFiles()
         );
+        $this->assertEquals('Dietcube', $app->getAppNamespace());
+        $this->assertEquals(false, $app->isDebug());
+
+        $app->initHttpRequest($container);
+        $this->assertEquals('www.dietcube.org', $app->getHost());
+        $this->assertEquals('80', $app->getPort());
+        $this->assertEquals('/documentation/setup', $app->getPath());
+        $this->assertEquals('http', $app->getProtocol());
+        $this->assertEquals('http://www.dietcube.org', $app->getUrl());
+
+        $this->assertEquals(dirname(__DIR__) . '/webroot', $app->getWebrootDir());
+        $this->assertEquals(__DIR__ . '/resource', $app->getResourceDir());
+        $this->assertEquals(__DIR__ . '/template', $app->getTemplateDir());
+        $this->assertEquals('.html.twig', $app->getTemplateExt());
+        $this->assertEquals(__DIR__ . '/config', $app->getConfigDir());
+        $this->assertEquals(dirname(__DIR__) . '/tmp', $app->getTmpDir());
+    }
+}
+
+class MockApplication extends Application
+{
+    public function config(Container $container)
+    {
     }
 }
