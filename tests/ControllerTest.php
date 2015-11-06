ee07f7bbc6315a0f3a784ac82950c6f7aa021318
Sotaro KARASAWA <sotaro.k@gmail.com>
2015-11-06 19:01:24 +0900

    Add controller test

---
+++ tests/ControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ *
+ */
+
+namespace Dietcube;
+
+use Pimple\Container;
+
+/**
+ * @backupGlobals
+ */
+class ControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetContainerValue()
+    {
+        $hoge = new \StdClass();
+        $container = self::getContainerAsFixture(['hoge' => $hoge]);
+        $controller = new Controller($container);
+
+        $method = $this->getInvokableMethod('get');
+        $this->assertSame($hoge, $method->invokeArgs($controller, ['hoge']));
+    }
+
+    public function testIsPost()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'post';
+        $container = self::getContainerAsFixture(['global.server' => new Parameters($_SERVER)]);
+        $controller = new Controller($container);
+
+        $method = $this->getInvokableMethod('isPost');
+        $this->assertTrue($method->invoke($controller));
+
+        $_SERVER['REQUEST_METHOD'] = 'get';
+        $container = self::getContainerAsFixture(['global.server' => new Parameters($_SERVER)]);
+        $controller = new Controller($container);
+
+        $method = $this->getInvokableMethod('isPost');
+        $this->assertFalse($method->invoke($controller));
+    }
+
+    public static function getInvokableMethod($method)
+    {
+        $class = new \ReflectionClass('\\Dietcube\\Controller');
+        $method = $class->getMethod($method);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+    public static function getContainerAsFixture(array $fixture = [])
+    {
+        $container = new Container();
+
+        foreach ($fixture as $key => $value) {
+            $container[$key] = $value;
+        }
+        return $container;
+    }
+}
